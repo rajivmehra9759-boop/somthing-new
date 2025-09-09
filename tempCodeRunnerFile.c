3dc9e33670fbaf15ce85ae736a7bda2b145de939
rajivmehra9759-boop <rajivmehra9759@gmail.com>
2025-09-09 20:53:40 +0530

    new

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    
+    for(int i = 1; i <= 5; i++) {
+        for(int j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n"); // Move to the next line after each row
+    }
+
+    return 0;
+}
