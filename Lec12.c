136976d011c837088eccc119eb30d69bb2917bac
rajivmehra9759-boop <rajivmehra9759@gmail.com>
2025-09-15 15:08:25 +0530

    mm

--- Lec12.c
+++ Lec12.c
@@ -1,9 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
+int main() {
+    int arr[5] = {1,2,3,4,4};
+    int sum =0;
+   
+    for (int i = 0; i < 5; i++) 
+    {
+        sum = sum + arr[i];
+    }
+    printf(" %d", sum);
+    return 0;
 
-int main(){
-    int arr[5]={1,2,3,4,5};
-    for(int i=0;i<5;i++){
-        printf("%d ",arr[i]);
-
-}
 }
