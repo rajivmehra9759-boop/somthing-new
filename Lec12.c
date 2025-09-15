84e5d86b58ed73bb9278a04cb13df799a3b72686
rajivmehra9759-boop <rajivmehra9759@gmail.com>
2025-09-15 15:17:30 +0530

    mmm

--- Lec12.c
+++ Lec12.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main() {
-    int arr[5] = {1,2,3,4,5};
-    int minimum = arr[0];
+    int arr[5] = {10,26,3,4,5};
+    int max = arr[0];
     for(int i=1; i<5; i++) {
-        if(arr[i] < minimum) {
-            minimum = arr[i];
+        if(arr[i] > max) {
+            max = arr[i];
         }
 
     }
-    printf("The minimum value is %d\n", minimum);
+    printf("The max value is %d\n", max);
     return 0;
 }
