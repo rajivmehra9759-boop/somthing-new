28d481602b137eca9b682b6c871e946a0984e401
rajivmehra9759-boop <rajivmehra9759@gmail.com>
2025-09-17 10:40:23 +0530

    hh

---
+++ cpp/new.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class Car{ 
+    public:
+    string brand;
+    string model;
+    
+
+    Car(){
+        brand = "Unknown ";
+        model =  "Unknown ";
+
+    }
+        Car(string b,string m){
+        brand=b;
+        model=m;
+        }
+     };
+  int main()
+  {
+  Car car1;
+  Car car2("BMW ","X5 ");
+  Car car3("FORD ","MUSTANG ");
+  cout<< "Car1:" << car1.brand <<""<< car1.model <<"\n";
+  cout<< "Car2:"<< car2.brand <<""<< car2.model <<"\n";                                         
+  cout<< "Car3:" << car3.brand <<""<< car3.model ;
+  return 0;}
